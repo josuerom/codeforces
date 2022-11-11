d17fea3cf44a0274849dc38da5d7aab06291bb36
Josué Romero <79048209+josuerom@users.noreply.github.com>
2022-11-11 17:55:57 -0500

    Mi plantilla de C++20

--- template.cpp
+++ template.cpp
@@ -1,11 +1,12 @@
-/******************************************
-* @AUTHOR:            JOSUEROM            *
-* @UNIVERSITY: MINUTE OF GOD OF COLOMBIA  *
-******************************************/
+/***************************************************
+* @AUTHOR:                JOSUEROM                 *
+* @DATE:               11/11/22 17:48              *
+* @INSTITUTE: MINUTE OF GOD UNIVERSITY OF COLOMBIA *
+***************************************************/
 #pragma GCC optimize("03,unroll-loops")
 #include <bits/stdc++.h>
 
-#define speed ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); cerr.tie(nullptr);
+#define SPEED_IO ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); cerr.tie(nullptr);
 #define all(vec) (vec).begin(), (vec).end()
 #define rall(vec) (vec).rbegin(), (vec).rend()
 #define only_decimals(n) cout.setf(ios::fixed, ios::floatfield); cout.precision(n);
@@ -15,17 +16,16 @@
 #define fore(e, vec) for (auto &(e): (vec))
 #define forr(i, vec) for(int i = (int)(vec).size()-1; i >= 0; i--)
 #define length(vec) (int)((vec).size())
-#define module(a, b, mod) ((((a) % (mod)) + ((b) % (mod))) % (mod))
+#define mod(a, b, mod) ((((a) % (mod)) + ((b) % (mod))) % (mod))
 #define binary_search(vec, key) sort(all(vec)); binary_search(all(vec), key);
 #define mcd(a, b) gcd(a, b)
 #define is_ordered(vec) is_heap(all(vec))
 #define read_array(vec) void template<typename T> void read(vector<T>& (vec)) { int n=(vec).size(); for(auto &e : (vec)) cin >> e; }
 #define program_time cerr << "finished in " << (float)clock() / (CLOCKS_PER_SEC/1000) << " ms\n"
-#define veredict(condition) cout << ((condition) ? "YES\n" : "NO\n") << ln
-#define online_jugde #ifndef ONLINE_JUDGE freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout); #endif
-#define test_cases(t) while((t)--) solve();
-#define ln "\n"
-
+#define veredict(condition) cout << ((condition) ? "YES\n" : "NO\n") << endl
+#define ONLINE_JUGDE #ifndef ONLINE_JUDGE freopen("inp.in", "r", stdin); freopen("out.ans", "w", stdout); #endif
+#define TEST_CASE(tc) while((tc)--) solve();
+#define endl "\n"
 #define MAX (ll) 1e9
 #define MIN (ll) 1e4
 #define PI 3.1415926535897932384626433832795
@@ -38,16 +38,16 @@ typedef unsigned int ui; //4^9
 typedef unsigned short us; //65^3
 
 ll n, m, t, k, c;
-//ll m[MAX][MAX], a[MIN];
+//ll M[MAX][MAX], A[MIN];
 
 void solve() {
-   // solution here
+   Solution Here!
 }
 
 int main() {
-   speed;
-   online_jugde;
-   scanf("%d", &t);
-   test_cases(t);
+   SPEDD_IO;
+   ONLINE_JUGDE;
+   cin >> t;
+   TEST_CASE(t);
    return 0;
 }
