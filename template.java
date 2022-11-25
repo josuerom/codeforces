2b7d2ca520a5011b24d5cf716de9e5cb9ec59932
Josué Romero <79048209+josuerom@users.noreply.github.com>
2022-11-24 21:37:22 -0500

    template.java

--- template.java
+++ template.java
@@ -1,1 +1,40 @@
+/*********************************************
+ * @author:           josuerom               *
+ * @created:     14/Nov/22 13:30:17          *
+*********************************************/
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
+public class SolutionA {
+   public static void main(String[] args) {
+      pc2Read();
+   }
+
+   public static void solve() {
+   }
+
+   public static void pc2Read() {
+      String vec[], line;
+      int arr[], tc;
+
+      BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+
+      try {
+         tc = Integer.valueOf(buffer.readLine());
+
+         while (tc-- > 0) {
+            line = buffer.readLine();
+            arr = Arrays.stream(line.substring(0).split(" ")).map(String::trim)
+               .mapToInt(Integer::parseInt).toArray();
+            solve(arr);
+         }
+         buffer.close();
+      } catch (NullPointerException e) {
+         e.printStackTrace();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+   }
+}
