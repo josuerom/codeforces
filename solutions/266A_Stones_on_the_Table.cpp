0ee2d90fe55ea8267301e0a0e9b0c78ba1ad6751
Josue Romero <josueromram3@gmail.com>
2023-04-11 23:33:56 -0500

    New updates

--- solutions/266A_Stones_on_the_Table.cpp
+++ solutions/266A_Stones_on_the_Table.cpp
@@ -1,15 +1,30 @@
-/******************************************
-* @AUTHOR:            JOSUEROM            *
-* @UNIVERSITY: MINUTE OF GOD OF COLOMBIA  *
-******************************************/
+/**
+ *  author:  josuerom
+ *  created: 11/04/23 22:01:29
+**/
 #pragma GCC optimize("03,unroll-loops")
-#include <iostream>
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#include <bits/stdc++.h>
 using namespace std;
 
+#ifdef LOCAL
+#include "data/debug.h"
+#else
+#define debug(...) 42
+#endif
+
 int main() {
-   short n, c; cin >> n;
-   string s; cin >> s;
-   for (int i = 0; i < n-1; i++) if (s[i]==s[i+1]) c++;
-   cout << c;
+   ios::sync_with_stdio(false);
+   cin.tie(0);
+#ifndef ONLINE_JUDGE
+   freopen("sample/inp.in", "r", stdin);
+   //freopen("sample/out.ans", "w", stdout);
+#endif
+   short n, ans = 0;
+   string s;
+   cin >> n >> s;
+   for (int i = 0; i < n-1; i++) if (s[i]==s[i+1]) ans++;
+   cout << ans << '\n';
    return 0;
 }
--- solutions/791A_Bear_and_Big_Brother.cpp
+++ solutions/791A_Bear_and_Big_Brother.cpp
@@ -1,17 +1,33 @@
-/******************************************
-* @AUTHOR:            JOSUEROM            *
-* @UNIVERSITY: MINUTE OF GOD OF COLOMBIA  *
-******************************************/
-#include <iostream>
+/**
+ *  author:  josuerom
+ *  created: 11/04/23 22:07:31
+**/
+#pragma GCC optimize("03,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#include <bits/stdc++.h>
 using namespace std;
 
+#ifdef LOCAL
+   #include "data/debug.h"
+#else
+   #define debug(...) 42
+#endif
+
 int main() {
-   short a, b, c;
+#ifndef ONLINE_JUDGE
+   //freopen("sample/input.in", "r", stdin);
+   //freopen("sample/out.ans", "w", stdout);
+#endif
+   ios::sync_with_stdio(false);
+   cin.tie(0);
+   int a, b, ans = 0;
    cin >> a >> b;
-   while (a <= b) {
+   while (true) {
+      if (a>b) break;
       a *= 3; b *= 2;
-      c++;
+      ans++;
    }
-   cout << c;
+   cout << (ans==0 ? 1 : ans) << '\n';
    return 0;
 }
