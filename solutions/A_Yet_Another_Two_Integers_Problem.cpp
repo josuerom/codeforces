0d6154c7a3b8589db05a3a678627cff8634d7f2e
Josue Romero <josueromram3@gmail.com>
2023-04-12 01:16:46 -0500

    New solutions

---
+++ solutions/A_Yet_Another_Two_Integers_Problem.cpp
@@ -0,0 +1,24 @@
+/**
+ *  author:  josuerom
+ *  created: 12/04/23 00:06:15
+**/
+#pragma GCC optimize("03,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(NULL);
+   //freopen("sample/input.in", "r", stdin);
+   int tt;
+   cin >> tt;
+   while (tt--) {
+      ll a, b, ans = 0;
+      cin >> a >> b;
+   }
+   return 0;
+}
