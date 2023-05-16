dad47fc3ba8860d7d13e8f8c3a6bc0eac6c9fec1
Josue Romero <josueromram3@gmail.com>
2023-05-16 13:00:36 -0500

    Update

--- solutions/1367B_Even_Array.cpp
+++ solutions/1367B_Even_Array.cpp
@@ -21,14 +21,5 @@ int main() {
       int n;
       cin >> n;
       vector<int> a(n);
-      bool flag = 1;
-      int good = 0, bad = 0;
-      for (int i = 0; i < n; i++) {
-         cin >> a[i];
-         if (i%2 != a[i]%2) { flag = 0; bad++; }
-         else good++;
-      }
-      debug(bad, good);
-      cout << (!flag && n==1 ? -1 : !flag ? abs(bad-good) : 0) << '\n';
    }
 }
