0a5ee3eec69f22787891aa62df951b370eb9bc43
Josue Romero <josueromram3@gmail.com>
2023-07-10 18:05:29 -0500

    Update file

--- solutions/A_Balanceo_Parentesis.java
+++ solutions/A_Balanceo_Parentesis.java
@@ -1,7 +1,7 @@
 /**
    ░░█ █▀█ █▀ █░█ █▀▀ █▀█ █▀█ █▀▄▀█
    █▄█ █▄█ ▄█ █▄█ ██▄ █▀▄ █▄█ █░▀░█
-   created: 30/06/23 10:47:10
+   created: 10/06/23 17:42:05
 **/
 import java.util.Scanner;
 import java.util.Stack;
@@ -13,25 +13,26 @@ public class A_Balanceo_Parentesis {
 
    static void solve(String[] s, int n) {
       Stack<String> pila = new Stack<>();
-      int index = 0;
+      int error = 0;
       for (int i = 0; i < n; i++) {
-         if (s[i].equals("(") || s[i].equals("{") || s[i].equals("[")) {
+         if (s[i].equals("(") || s[i].equals("{") || s[i].equals("[") || s[i].equals("<")) {
             pila.push(s[i]);
-            index = i;
-         } else if (pila.empty() && (s[i].equals(")") || s[i].equals("}") || s[i].equals("]"))) {
+            error = i;
+         } else if (pila.empty() && (s[i].equals(")") || s[i].equals("}") || s[i].equals("]") || s[i].equals(">"))) {
             System.out.println("Error en " + i);
             return;
-         } else if (s[i].equals(")") || s[i].equals("}") || s[i].equals("]")) {
+         } else if (s[i].equals(")") || s[i].equals("}") || s[i].equals("]") || s[i].equals(">")) {
             if (s[i].equals(")") && pila.peek().equals("(")) pila.pop();
             else if (s[i].equals("}") && pila.peek().equals("{")) pila.pop();
             else if (s[i].equals("]") && pila.peek().equals("[")) pila.pop();
+            else if (s[i].equals(">") && pila.peek().equals("<")) pila.pop();
             else {
                System.out.println("Error en " + i);
                return;
             }
          }
       }
-      System.out.println((pila.size() == 1) ? "Error en " + index : "Balanceada");
+      System.out.println((pila.size() > 0) ? "Error en " + error : "Balanceada");
    }
 
    static void readInput() {
