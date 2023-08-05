e8d744cfb16d4f1e4bd16ae4f8b77df8f0c9166e
Josue Romero <josueromram3@gmail.com>
2023-08-04 20:06:23 -0500

    Co-authored-by: Segura Linares Estefany Carolina <estefany.segura@uniminuto.edu.co>

--- solutions/A_Translation.java
+++ solutions/A_Translation.java
@@ -1,3 +1,4 @@
+
 /**
  *   author:  josuerom
  *   created: 06/06/23 10:14:16
@@ -20,9 +21,10 @@ public class A_Translation {
       char t[] = fr.next().toCharArray();
       int n = s.length, k = t.length;
       boolean ans = true;
-      if (n != k) ans = false;
+      if (n != k)
+         ans = false;
       else {
-         for (int i = 0, j = n-1; i < n; i++, j--) {
+         for (int i = 0, j = n - 1; i < n; i++, j--) {
             if (s[i] != t[j]) {
                ans = false;
                break;
@@ -30,7 +32,8 @@ public class A_Translation {
          }
       }
       pw.println((ans ? "YES" : "NO"));
-      pw.flush(); pw.close();
+      pw.flush();
+      pw.close();
    }
 
    static final Random random = new Random();
@@ -39,30 +42,37 @@ public class A_Translation {
    static final boolean DEBUG = false;
 
    static class Treasure {
-      public Treasure() { }
+      public Treasure() {
+      }
 
       static void ruffleSort(int[] a) {
          int n = a.length;
          for (int i = 0; i < n; i++) {
             int oi = random.nextInt(n), temp = a[oi];
-            a[oi] = a[i]; a[i] = temp;
+            a[oi] = a[i];
+            a[i] = temp;
          }
          Arrays.sort(a);
       }
 
       static long add(long a, long b) {
-         return (a+b) % MOD;
+         return (a + b) % MOD;
       }
+
       static long sub(long a, long b) {
-         return ((a-b) % MOD + MOD) % MOD;
+         return ((a - b) % MOD + MOD) % MOD;
       }
+
       static long mul(long a, long b) {
-         return (a*b) % MOD;
+         return (a * b) % MOD;
       }
+
       static long exp(long base, long exp) {
-         if (exp==0) return 1;
-         long half=exp(base, exp/2);
-         if (exp%2==0) return mul(half, half);
+         if (exp == 0)
+            return 1;
+         long half = exp(base, exp / 2);
+         if (exp % 2 == 0)
+            return mul(half, half);
          return mul(half, mul(half, base));
       }
 
@@ -71,12 +81,15 @@ public class A_Translation {
 
       static void precompFacts() {
          factorials[0] = invFactorials[0] = 1;
-         for (int i = 1; i < factorials.length; i++) factorials[i] = mul(factorials[i-1], i);
-         invFactorials[factorials.length-1] = exp(factorials[factorials.length-1], MOD-2);
-         for (int i = invFactorials.length-2; i >= 0; i--) invFactorials[i] = mul(invFactorials[i+1], i+1);
+         for (int i = 1; i < factorials.length; i++)
+            factorials[i] = mul(factorials[i - 1], i);
+         invFactorials[factorials.length - 1] = exp(factorials[factorials.length - 1], MOD - 2);
+         for (int i = invFactorials.length - 2; i >= 0; i--)
+            invFactorials[i] = mul(invFactorials[i + 1], i + 1);
       }
+
       static long nCr(int n, int r) {
-         return mul(factorials[n], mul(invFactorials[r], invFactorials[n-r]));
+         return mul(factorials[n], mul(invFactorials[r], invFactorials[n - r]));
       }
    }
 
@@ -91,24 +104,42 @@ public class A_Translation {
 
       String next() {
          while (!st.hasMoreTokens()) {
-            try { st = new StringTokenizer(br.readLine()); }
-            catch (IOException e) { e.printStackTrace(); }
+            try {
+               st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+               e.printStackTrace();
+            }
          }
          return st.nextToken();
       }
+
       String readLine() {
          String line = null;
-         try { line = br.readLine(); }
-         catch (IOException e) { e.printStackTrace(); }
+         try {
+            line = br.readLine();
+         } catch (IOException e) {
+            e.printStackTrace();
+         }
          return line;
       }
 
-      double readDouble() { return Double.parseDouble(next()); }
-      long readLong() { return Long.parseLong(next()); }
-      int readInt() { return Integer.parseInt(next()); }
+      double readDouble() {
+         return Double.parseDouble(next());
+      }
+
+      long readLong() {
+         return Long.parseLong(next());
+      }
+
+      int readInt() {
+         return Integer.parseInt(next());
+      }
+
       int[] readArray(int n) {
          int[] a = new int[n];
-         for (int i = 0; i < n; i++) { a[i] = readInt(); }
+         for (int i = 0; i < n; i++) {
+            a[i] = readInt();
+         }
          return a;
       }
    }
