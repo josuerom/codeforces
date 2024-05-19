e0144f32699603c2b628f1a9206ccd940c10edc6
josuerom <josue.romero@uniminuto.edu.co>
2024-05-18 21:30:46 -0500

    New solutions

--- src/900/10512_Visitando_una_lista_con_modificaciones.py
+++ src/900/10512_Visitando_una_lista_con_modificaciones.py
@@ -1,32 +1,34 @@
 """
    author:  josuerom
-   created: 23/04/24 16:41:22
+   created: 16/05/24 22:03:57
 """
 import sys
-import math
 
-def rli(): return list(map(int, sys.stdin.readline().split()))
-def rls(): return list(map(str, sys.stdin.readline().split(r"\s+")))
-def rlc(): return sys.stdin.readline().strip()
-def rint(): return int(sys.stdin.readline())
+def rstr(): return sys.stdin.readline()
+def rint(): return int(rstr())
+def rli():  return list(map(int, rstr().split()))
+def rls():  return list(map(str, rstr().split(r"\s+")))
+def rlc():  return list(rstr())
 def pl(ls): print(' '.join(map(str, ls)))
-def plb(ls): print('\n'.join(map(str, ls)))
+def pb(ls): print('\n'.join(map(str, ls)))
 
-n, m = rli()
+n, m = map(int, input().split())
 a = [i for i in range(n)]
-i = 0
+i, cnt = 0, 0
 for _ in range(m):
-   c, *ln = rlc().split()
-   if c.upper().startswith("A"):
-      x, sz  = ln[0], len(a)
-      if x <= 0:
-         x *= 1
-         i = math.fabs(i - x)
-      elif i + x >= sz:
-         i = sz - x
+   ln = rls()
+   if ln[0].lower().startswith("a"):
+      i += cnt
+      k = int(ln[1])
+      if k < 0:
+         i -= abs(k)
       else:
-         i += x
-   elif c.upper().startswith("I"):
-      pass
-   elif c.upper().startswith("E"):
-      pass
+         i += k
+      cnt = 0
+   elif ln[0].lower().startswith("i"):
+      a.insert(i - 1, int(ln[1]))
+      cnt += 1
+   elif ln[0].lower().startswith("e"):
+      if len(a):
+         a.pop(i - 1)
+print(a)
